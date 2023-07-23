7272cc769fd655cdda897e43b35b33c9a369f8cd
Hugo Marcelino <hugo.m.marcelino@gmail.com>
2023-07-23 18:55:04 +0100

    Parses definition and retrieves information about var and calculation expressions

--- integrationm/common/com/cultofbits/customizations/calc/DefinitionCalculator.groovy
+++ integrationm/common/com/cultofbits/customizations/calc/DefinitionCalculator.groovy
@@ -1,16 +1,18 @@
 package com.cultofbits.customizations.calc
 
 import com.cultofbits.integrationm.service.dictionary.recordm.Definition
+import com.cultofbits.integrationm.service.dictionary.recordm.FieldDefinition
+
 class DefinitionCalculator {
 
-    def defName;
-    def defVersion;
+    protected String defName;
+    protected Integer defVersion;
 
     // a map with the key the var name (including var.) and the value the list of all fields
-    def fdVarByVarName = [:]
+    protected Map<String, List<FieldDefinition>> fdVarByVarName = [:]
 
     // All field definitions that have $calc.<operation>
-    def fdExprById = [:]
+    protected Map<Integer, CalcExpr> fdExprById = [:]
 
     DefinitionCalculator(definition) {
         this.defName = definition.name
@@ -24,34 +26,49 @@ class DefinitionCalculator {
 
         // Collect all field definitions with $var
         fdVarByVarName = definition.fieldDefinitions
-                .findAll { fId, f -> f.description?.toString() =~ /.*[$]var.*/ }
+                .findAll { fd -> fd.description?.indexOf("\$var.") != -1 }
+                .inject([:] as Map<String, List<FieldDefinition>>) { map, fd ->
+                    def words = fd.description =~ /([^\s]+)/
+                    def varArg = (0..words.getCount() - 1)
+                            .findResult { i -> words[i][0].indexOf("\$var") != -1 ? words[i][0] : null }
+                            .substring(1) // remove the initial $
+
+                    def varArgMapEntry = map[varArg]
+                    if (varArgMapEntry != null) {
+                        varArgMapEntry << fd
+                    } else {
+                        map << [(varArg): [fd]]
+                    }
+
+                    map
+                }
 
         // Collect all field definitions with $calc.<operation>
         fdExprById = definition.fieldDefinitions
-                .findAll { fd -> fd.description.toString() =~ /[$]calc\./ }
-                .inject([:]) { map, fd ->
+                .findAll { fd -> fd.description =~ /[$]calc\./ }
+                .inject([:] as Map<Integer, CalcExpr>) { map, fd ->
                     def op = (fd.description =~ /.*[$]calc.([^(]+)/).with { it[0][1] }
                     def args = (fd.description =~ /.*[$]calc.[^(]+\(([^(]+)\)/).with { it[0][1].tokenize(",") }
 
-                    map << [(fd.id): new CalcExpr().tap {
+                    map << [(fd.id): new CalcExpr().with {
                         it.operation = op
                         it.args = args
+                        it
                     }]
                 }
     }
 
-    Optional<Map<String, String>> calculate() {
+    Optional<Map<String, String>> calculate(eventMsg) {
 
     }
 
-    class CalcExpr {
+    static class CalcExpr {
         // one of: sum, multiply, difference, divide
-        def operation;
+        String operation;
 
         // All the arguments from the $calc expression like var.xxxx or any constant value
-        def args;
+        List<String> args;
+
     }
 
 }
-
-import com.cultofbits.integrationm.service.dictionary.recordm.FieldDefinition
--- others/test/groovy/com/cultofbits/customizations/calc/DefinitionCalculatorTest.groovy
+++ others/test/groovy/com/cultofbits/customizations/calc/DefinitionCalculatorTest.groovy
@@ -1,11 +1,125 @@
 package com.cultofbits.customizations.calc
 
+import com.cultofbits.integrationm.service.dictionary.recordm.Definition
+import com.cultofbits.integrationm.service.dictionary.recordm.FieldDefinition
 import org.junit.Test
+
+import static org.junit.Assert.assertEquals
 
 class DefinitionCalculatorTest {
 
     @Test
-    void smokeTest() {
-        println(("\$calc.multiply(var.a, var.b, 10)" =~ /.*[$]calc.[^(]+\(([^(]+)\)/).with { it[0][1].tokenize(",") })
+    void validate_var_regex() {
+        Definition definition = new Definition().with {
+            it.name = "definition 1"
+            it.version = 1
+            it.fieldDefinitions = [
+                    new FieldDefinition().with {
+                        it.id = 1; it.name = "field1";
+                        it.description = "\$var.field1 dummy text";
+                        it
+                    },
+                    new FieldDefinition().with {
+                        it.id = 2; it.name = "field2";
+                        it.description = "\$number(2) \$var.field2 \$hint[some hint]";
+                        it
+                    },
+                    new FieldDefinition().with {
+                        it.id = 3; it.name = "field3";
+                        it.description = "\$var.some.var.structure \$number(2) \$hint[some hint]";
+                        it
+                    },
+                    new FieldDefinition().with {
+                        it.id = 4; it.name = "field4";
+                        it.description = "\$number(2) \$hint[some hint] \$var.some.var.structure ";
+                        it
+                    }
+            ]
+            it
+        }
+
+        def calculator = new DefinitionCalculator(definition)
+
+        assertEquals(calculator.fdVarByVarName["var.field1"].collect {it.id}.join(","), "1")
+        assertEquals(calculator.fdVarByVarName["var.field2"].collect {it.id}.join(","), "2")
+        assertEquals(calculator.fdVarByVarName["var.some.var.structure"].collect {it.id}.join(","), "3,4")
     }
+
+    @Test
+    void can_process_simple_relations_with_var_and_calc_expression() {
+        Definition definition = new Definition().with {
+            it.name = "definition 1"
+            it.version = 1
+            it.fieldDefinitions = [
+                    new FieldDefinition().with {
+                        it.id = 1; it.name = "field1";
+                        it.description = "\$var.field1";
+                        it
+                    },
+                    new FieldDefinition().with {
+                        it.id = 2; it.name = "field2";
+                        it.description = "\$var.field2";
+                        it
+                    },
+                    new FieldDefinition().with {
+                        it.id = 3; it.name = "field1";
+                        it.description = "\$calc.multiply(var.field1,var.field2,10)";
+                        it
+                    }
+            ]
+            it
+        }
+
+        def calculator = new DefinitionCalculator(definition)
+        assertEquals(calculator.defName, definition.name)
+        assertEquals(calculator.defVersion, 1)
+
+        assertEquals(calculator.fdVarByVarName["var.field1"], [definition.fieldDefinitions[0]])
+        assertEquals(calculator.fdVarByVarName["var.field2"], [definition.fieldDefinitions[1]])
+
+        assertEquals(calculator.fdExprById[3].operation, "multiply")
+        assertEquals(calculator.fdExprById[3].args, ["var.field1", "var.field2", "10"])
+    }
+
+    @Test
+    void can_process_simple_relations_with_var_and_calc_with_dependencies() {
+        Definition definition = new Definition().with {
+            it.name = "definition 1"
+            it.version = 1
+            it.fieldDefinitions = [
+                    new FieldDefinition().with {
+                        it.id = 1; it.name = "field1";
+                        it.description = "\$var.field1";
+                        it
+                    },
+                    new FieldDefinition().with {
+                        it.id = 2; it.name = "field2";
+                        it.description = "\$var.field2 \$calc.sum(var.field1,1000)";
+                        it
+                    },
+                    new FieldDefinition().with {
+                        it.id = 3; it.name = "field1";
+                        it.description = "\$calc.multiply(var.field1,var.field2,10)";
+                        it
+                    }
+            ]
+            it
+        }
+
+        def calculator = new DefinitionCalculator(definition)
+        assertEquals(calculator.defName, definition.name)
+        assertEquals(calculator.defVersion, 1)
+
+        assertEquals(calculator.fdVarByVarName["var.field1"], [definition.fieldDefinitions[0]])
+        assertEquals(calculator.fdVarByVarName["var.field2"], [definition.fieldDefinitions[1]])
+
+        assertEquals(calculator.fdExprById[2].operation, "sum")
+        assertEquals(calculator.fdExprById[2].args, ["var.field1", "1000"])
+
+        assertEquals(calculator.fdExprById[3].operation, "multiply")
+        assertEquals(calculator.fdExprById[3].args, ["var.field1", "var.field2", "10"])
+    }
+
+
+
 }
