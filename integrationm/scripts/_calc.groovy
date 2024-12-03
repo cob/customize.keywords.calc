5d656a0af622a88d478391bb46e149b73b6e5bff
Hugo Marcelino <hugo.m.marcelino@gmail.com>
2024-12-03 15:56:34 +0000

    Clean null values before updating a record

--- integrationm/scripts/_calc.groovy
+++ integrationm/scripts/_calc.groovy
@@ -6,6 +6,7 @@ if (msg.user == "integrationm" || msg.product != "recordm" || !(msg.action =~ "a
 
 def updateMap = CalculatorsDefinitionCache.getCalculatorForDefinition(msg, recordm, log)
         .calculate(msg)
+        .findAll { it.value != null }
 
 if (updateMap.size() > 0) {
     recordm.update(msg.type, msg.instance.id, updateMap);
